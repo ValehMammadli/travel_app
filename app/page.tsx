fb380881f181373c786b83558a527f21ecb2edae
Valeh Mammadli <valehlothbrok@gmail.com>
2024-02-09 18:12:55 +0400

    "Add Camp component to page and update Home component to include it"

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import Camp from "@/components/Camp";
 import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <Hero/>
+    <>
+      <Hero />
+      <Camp />
+    </>
   );
 }
